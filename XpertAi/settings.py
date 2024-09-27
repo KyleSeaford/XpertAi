15bca9422ac4e9cf847df07a4b711cfc557b494f
Kyle Seaford <kyleseaford5@gmail.com>
2024-09-27 11:33:40 +0100

    Refactor static file paths and update project name to XpertAi

--- XpertAi/settings.py
+++ XpertAi/settings.py
@@ -38,23 +38,9 @@ ALLOWED_HOSTS = ['*']
 
 import os
 
-# Define the URL to access static files
-STATIC_URL = '/static/'
-
-# Define the directory to collect static files in production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Optional: Additional static directories
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-MIDDLEWARE = [
-    # Add WhiteNoise middleware right after the SecurityMiddleware
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    # other middleware...
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Enable gzip compression for static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -108,11 +94,14 @@ WSGI_APPLICATION = 'XpertAi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<mydb>',
+        'USER': '<myuser>',
+        'PASSWORD': '<mypass>',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- XpertAi/urls.py
+++ XpertAi/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from XpertAi import settings
 
 from . import views
 
@@ -28,3 +31,6 @@ urlpatterns = [
     path('chat-api/', views.chat_api, name='chat_api'),
     path('feedback-api/', views.feedback_api, name='feedback_api'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
