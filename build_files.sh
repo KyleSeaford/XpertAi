8b25653fd42442e17927b1752367da798d513e23
Kyle Seaford <kyleseaford5@gmail.com>
2024-09-28 22:34:25 +0100

    Refactor build_files.sh and vercel.json for improved installation and static files handling

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-echo "Installing requirements"
-pip install -r requirements.txt
-echo "Running migrations"
-python manage.py migrate
-echo "Collecting static files"
-python manage.py collectstatic --noinput
