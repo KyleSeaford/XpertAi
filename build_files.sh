aee0783ddb73c3456c6a242a7f5a77b558e9048a
Kyle Seaford <kyleseaford5@gmail.com>
2024-09-28 22:31:26 +0100

    Refactor build_files.sh to improve installation and static files handling

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "Installing requirements"
-pip install -r requirements.txt
+pip install -r requirements.txt || exit 1
 echo "Running migrations"
-python3.9 manage.py migrate
+python manage.py migrate || exit 1
 echo "Collecting static files"
-python3.9 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput || exit 1
